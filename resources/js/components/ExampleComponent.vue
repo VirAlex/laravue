199ab5bbd4ee8fdee9f31c4cc7d1478302d3edd0
VirAlex <alexou73@gmail.com>
2020-03-07 09:55:55 +0100

    init

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex">
+      <p>
+        <input type="text" v-model="name">
+        <input type="checkbox" v-model="checkbox">
+      </p>
+      <p>Bonjour {{name}} ! </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data(){
+          return{
+            name: "Alex",
+            checkbox: true
+          }
+        },
+    }
+</script>
